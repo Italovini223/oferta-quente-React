8a9516ea18c307e041174fdeec57657e889d5981
italo <italovinicius2018@gmail.com>
2022-09-27 20:24:29 -0300

    ✨ feat: add Input in Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,16 @@
+import { Input } from "../Input";
 import { Container } from "./styles";
+
+import {FiSearch} from 'react-icons/fi'
 
 export function Home(){
   <Container>
-    
+    <Top>
+      <Input 
+        type="text"
+        placeholder="Digite Sua busca"
+        icon={FiSearch}
+      />
+    </Top>
   </Container>
 }

d172687b7218745ffa2c5978b1d800f2ee476a85
Italovini223 <italovinicius2018@gmail.com>
2022-11-12 14:23:50 -0300

    🚧 wip: creating details page

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,9 +1,13 @@
-import { Container } from "../../components/Filter/styles";
+import { Container, Header } from "./styles";
 
+import logoImg from '../../assets/oferta quente branco.svg'
 export function Details() {
   return (
     <Container>
-      
+     <Header>
+      <img src={logoImg} alt="" />
+      <h1>Oferta Quente</h1>
+     </Header>
     </Container>
   )
 }
--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -1,6 +1,39 @@
 import styled from 'styled-components';
 
-const Container = styled.div`
+export const Container = styled.div`
   width: 100%;
   height: 100vh;
+
+  display: grid;
+  grid-template-rows: 150px auto;
+  grid-template-areas:
+  "header"
+  "content";
+  
+  background-color: ${({theme}) => theme.COLORS.BACKGROUND_900};
+`;
+
+export const Header = styled.header`
+  width: 100%;
+  height: 150px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+  
+  grid-area: header;
+
+  background-color: ${({theme}) => theme.COLORS.WHITE};
+  
+  
+  > img {
+
+    width: 60px;
+    height: 60px;
+  }
+
+  > span {
+
+  }
 `;

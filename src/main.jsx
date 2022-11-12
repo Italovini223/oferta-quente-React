e12beee7805cc3d17616d9abea0cd3cbeda0bb97
Italovini223 <italovinicius2018@gmail.com>
2022-11-12 11:19:35 -0300

    ✨ feat: add routes to app

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,17 @@ import ReactDOM from 'react-dom/client'
 
 import { ThemeProvider } from 'styled-components'
 
-import {Home} from './pages/Home'
 import GlobalStyles from './styles/global'
 import theme from './styles/theme'
+
+import {Routes} from './Routes'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Home />
+      <Routes />
     </ThemeProvider>
   </React.StrictMode>
 )
---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,9 @@
+import { Container } from "../../components/Filter/styles";
+
+export function Details() {
+  return (
+    <Container>
+      
+    </Container>
+  )
+}
